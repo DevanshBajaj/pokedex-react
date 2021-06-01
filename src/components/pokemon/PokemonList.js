9459c4746a088576d228bf7827a8e76ce71a8138
Devansh Bajaj <devansh21db.db@gmail.com>
2021-06-01 17:23:06 +0530

    added names and url to the card

--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -1,16 +1,38 @@
-import React from 'react';
-import PokemonCard from './PokemonCard';
+import React, { Component } from "react";
+import axios from "axios";
 
-const PokemonList = () => {
-    return (
-        <div className="row">
-            <PokemonCard />
-            <PokemonCard />
-            <PokemonCard />
-            <PokemonCard />
-            <PokemonCard />
-        </div>
-    )
+import PokemonCard from "./PokemonCard";
+
+class PokemonList extends Component {
+	state = {
+		url: "https://pokeapi.co/api/v2/pokemon/?limit=40",
+		pokemon: null,
+	};
+
+	async componentDidMount() {
+		const res = await axios.get(this.state.url);
+		this.setState({ pokemon: res.data['results'] });
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				{this.state.pokemon ? (
+					<div className="row">
+						{this.state.pokemon.map(pokemon => (
+							<PokemonCard
+								key={Math.random().toString()}
+								name={pokemon.name}
+								url={pokemon.url}
+							/>
+						))}
+					</div>
+				) : (
+					<h1>Loading Pokemon!</h1>
+				)}
+			</React.Fragment>
+		);
+	}
 }
 
-export default PokemonList
+export default PokemonList;
