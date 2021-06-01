e7de4d2ae2c8ee801bb457bf34ae0e92f78f2fd3
Devansh Bajaj <devansh21db.db@gmail.com>
2021-06-01 17:56:22 +0530

    styling card

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,23 +1,42 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class PokemonCard extends Component {
-    state = {
-        name: '',
-        imageUrl: '',
-        pokemonIndex: ''
-    };
-    render() {
-    const {name, url} = this.props;
-        return (
-            <div className="col-md-3 col-sm-6 mb-5">
-                <div className="card"></div>
-                <div className="card-header">
-                    <h1>{name}</h1>
-                </div>
-            </div>
-        )
-    }
+	state = {
+		name: "",
+		imageUrl: "",
+		pokemonIndex: "",
+	};
+
+	componentDidMount() {
+		const { name, url } = this.props;
+		const pokemonIndex = url.split("/")[url.split("/").length - 2];
+		const imageUrl = `https://github.com/PokeAPI/sprits/blob/master/sprites/pokemon/${pokemonIndex}`;
+		this.setState({
+			name,
+			imageUrl,
+			pokemonIndex,
+		});
+	}
+
+	render() {
+		return (
+			<div className="col-md-3 col-sm-6 mb-5">
+				<div className="card"></div>
+				<h5 className="card-header">{this.state.pokemonIndex}</h5>
+				<div className="card-body mx-auto">
+					<h6 className="card-title">
+						{this.state.name
+							.toLowerCase()
+							.split(" ")
+							.map(
+								(letter) => letter.charAt(0).toUpperCase() + letter.substring(1)
+							)
+							.join(" ")}
+					</h6>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default PokemonCard
+export default PokemonCard;
