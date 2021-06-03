c251a76060a2510849b0e4fb0fc50dcef39da0ff
Devansh Bajaj <devansh21db.db@gmail.com>
2021-06-03 17:52:23 +0530

    adding more data from the api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {HashRouter, HashRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import './App.css';
--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -1,33 +1,103 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-
+import React, { Component } from "react";
+import axios from "axios";
 
 class Pokemon extends Component {
-    state = {
-        name: '',
-        pokemonIndex: '',
-        imageUrl: ''
-    };
+	state = {
+		name: "",
+		pokemonIndex: "",
+		imageUrl: "",
+		types: [],
+		description: "",
+		stats: {
+			hp: "",
+			attack: "",
+			defense: "",
+			speed: "",
+			specialAttack: "",
+			specialDefence: "",
+		},
+		height: "",
+		weight: "",
+		eggGroup: "",
+		abilities: "",
+		genderRatioMale: "",
+		genderRatioFemale: "",
+		evs: "",
+		hatchSteps: "",
+	};
 
-    async componentDidMount() {
-        const { pokemonIndex } = this.props.match.params;
+	async componentDidMount() {
+		const { pokemonIndex } = this.props.match.params;
 
-        //URLs for information 
-        const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}/`;
-        const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}/`;
+		//URLs for information
+		const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}/`;
+		const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}/`;
 
-        const pokemonRes = await axios.get(pokemonUrl);
+		const pokemonRes = await axios.get(pokemonUrl);
 
-        const name = pokemonRes.data.name;
+		const name = pokemonRes.data.name;
+		const imageUrl = pokemonRes.data.sprites.front_default;
 
-        this.setState({ name })
-    }
-    render() {
-        return (
-            <div>
-                <h1>{this.state.name}</h1>
-            </div>)
-    }
+		let { hp, attack, defense, speed, specialAttack, specialDefence } = " ";
+
+		pokemonRes.data.stats.map((stat) => {
+			// eslint-disable-next-line default-case
+			switch (stat.stat.name) {
+				case "hp":
+					hp = stat["base_stat"];
+					break;
+				case "attack":
+					attack = stat["base_stat"];
+					break;
+				case "defense":
+					defense = stat["base_stat"];
+					break;
+				case "speed":
+					speed = stat["base_stat"];
+					break;
+				case "special-attack":
+					specialAttack = stat["base_stat"];
+					break;
+				case "special-defence":
+					specialDefence = stat["base_stat"];
+					break;
+			}
+		});
+
+		// convert decimeter to ft
+		const height =
+			Math.round((pokemonRes.data.height * 0.328084 + 0.0001) * 100) / 100;
+		const weight = Math.round(pokemonRes.data.weight / 10);
+
+		const types = pokemonRes.data.types.map((type) => type.type.name);
+		const abilities = pokemonRes.data.abilities.map((ability) => {
+			return ability.ability.name
+				.toLowerCase()
+				.split('-')
+				.map((s) => s.charAt(0).toUpperCase() + s.sub(1))
+				.join(" ");
+		});
+
+        const evs = pokemonRes.data.stats.filter(stat => {
+            if (stat.effort > 0) {
+                return true;
+            }
+            return false;
+        }).map(stat => {
+            return `${stat.effort} ${stat.name}`
+            .toLowerCase()
+            .split('-')
+            .map((s) => s.charAt(0).toUpperCase() + s.sub(1))
+            .join(" ");
+        }).join(' ');
+	}
+	render() {
+		return (
+			<div>
+				<h1>{this.state.name}</h1>
+			</div>
+		);
+	}
 }
 
 export default Pokemon;
