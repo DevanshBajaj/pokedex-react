0d9be05b97eaa7931d6d6c8eca02681112df6d88
Devansh Bajaj <devansh21db.db@gmail.com>
2021-06-10 01:05:45 +0530

    convert a class component to functional component

--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
+import React, { useState,useEffect} from "react";
 import axios from "axios";
 
 import PokemonCard from "./PokemonCard";
 
+/*
 class PokemonList extends Component {
 	state = {
 		url: "https://pokeapi.co/api/v2/pokemon/?limit=40",
@@ -34,5 +35,38 @@ class PokemonList extends Component {
 		);
 	}
 }
+*/
 
+const PokemonList = (props) => {
+	const [url, setUrl] = useState({
+		pokemon: null,
+	});
+
+	useEffect(() => {
+		const fetchData = async () => {
+			let res = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=40");
+			setUrl({ ...url, pokemon: res.data.results });
+		}
+		fetchData()
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+
+	return (
+		<React.Fragment>
+			{url.pokemon ? (
+				<div className="row">
+					{url.pokemon.map(pokemon => (
+						<PokemonCard
+							key={Math.random().toString()}
+							name={pokemon.name}
+							url={pokemon.url}
+						/>
+					))}
+				</div>
+			) : (
+				<h1>Loading Pokemon!</h1>
+			)}
+		</React.Fragment>
+	);
+}
 export default PokemonList;
