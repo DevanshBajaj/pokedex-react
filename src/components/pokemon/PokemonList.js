0079f8a1f54d977daf0eb521750e0cdb05ae540e
Devansh Bajaj <devansh21db.db@gmail.com>
2021-07-01 18:49:09 +0530

    added searchbar and more pokemons to search

--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -1,7 +1,7 @@
 import React, { useState,useEffect, lazy, Suspense} from "react";
 import axios from "axios";
 import spinner from './spinner.gif';
-
+import styles from './PokemonList.module.css'
 
 const PokemonCard = lazy(() => import('./PokemonCard'))
 
@@ -10,21 +10,36 @@ const PokemonList = (props) => {
 	const [url, setUrl] = useState({
 		pokemon: null,
 	});
+	const [filter, setFilter] = useState('')
+
 
 	useEffect(() => {
 		const fetchData = async () => {
-			let res = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=60");
+			let res = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=300");
 			setUrl({ ...url, pokemon: res.data.results });
 		}
 		fetchData()
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 
+	const handleSearchChange = (event) => {
+		setFilter(event.target.value)
+	}
+
 	return (
 		<React.Fragment>
+			<div className={styles.searchbox}>
+				<input 
+					type="text" 
+					placeholder="Search name" 
+					onChange={handleSearchChange}
+				/>
+        	</div>
 			{url.pokemon ? (
 				<div className="row">
-					{url.pokemon.map(pokemon => (
+					{url.pokemon.map(pokemon =>  
+						pokemon.name.includes(filter) &&
+						(
 						<Suspense key={Math.random().toString()} fallback={<h1 style={{color: 'rgb(224, 117, 133)'}}>Loading List....</h1>}>
 							<PokemonCard
 							key={Math.random().toString()}
@@ -32,7 +47,6 @@ const PokemonList = (props) => {
 							url={pokemon.url}
 						/>
 						</Suspense>
-						
 					))}
 				</div>
 			) : (
