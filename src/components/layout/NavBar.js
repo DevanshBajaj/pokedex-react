db35ae34d20ed66b68ed0e2720887f69be72fc9c
Devansh Bajaj <devansh21db.db@gmail.com>
2021-07-03 04:12:03 +0530

    customize navbar

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react';
 import styles from './NavBar.module.css';
-
+import Pokeball from './pokeball.svg';
 
 const NavBar = () => {
   return (
     <Fragment>
       <nav className={styles.navbar} >
+        <img src={Pokeball} alt="logo.svg"/>
         <a href="/">POKEDEX</a>
       </nav>
     </Fragment>
